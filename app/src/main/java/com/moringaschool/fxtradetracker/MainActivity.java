33c670e93c6a8a432cc425a04c2f3a8148208667
Cyprian Bowen <branco1lelgo@gmail.com>
2019-12-16 10:29:03 +0300

    add background image

--- app/src/main/java/com/moringaschool/fxtradetracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/MainActivity.java
@@ -3,8 +3,12 @@ package com.moringaschool.fxtradetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findFormButton) Button mFindFormButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
