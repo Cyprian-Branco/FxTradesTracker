438e939bb6a2c97f8b2910fc600fefdb736b0b97
Cyprian Bowen <branco1lelgo@gmail.com>
2019-12-16 16:07:26 +0300

    add onCreate method

--- app/src/main/java/com/moringaschool/fxtradetracker/Amount.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/Amount.java
@@ -1,4 +1,17 @@
 package com.moringaschool.fxtradetracker;
 
-public class Amount {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Amount extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculated_amount);
+    }
+
+
 }
