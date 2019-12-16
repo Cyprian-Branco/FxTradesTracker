039ef49e331df1fe320f552364b84a6fc83e9f28
Cyprian Bowen <branco1lelgo@gmail.com>
2019-12-16 11:37:23 +0300

    will add implementation of onclick interface

--- app/src/main/java/com/moringaschool/fxtradetracker/Trade_details.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/Trade_details.java
@@ -1,4 +1,38 @@
 package com.moringaschool.fxtradetracker;
 
-public class trade_details  {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Trade_details extends AppCompatActivity implements View.OnClickListener  {
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.calculateFormButton) Button mCalculateFormButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_details);
+        ButterKnife.bind(this);
+
+        mCalculateFormButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick (View v){
+        if (v == mCalculateFormButton){
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent (Trade_details.this, Amount.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+
+    }
+
 }
