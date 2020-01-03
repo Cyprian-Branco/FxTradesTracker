6387ce877ed5f0e6af8881648cbae798149191d5
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-03 20:22:23 +0300

    add toolbar logic

--- app/src/main/java/com/moringaschool/fxtradetracker/models/MainActivity.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/models/MainActivity.java
@@ -1,10 +1,14 @@
-package com.moringaschool.fxtradetracker;
+package com.moringaschool.fxtradetracker.models;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.moringaschool.fxtradetracker.R;
+import com.moringaschool.fxtradetracker.Trade_details;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void goTotrade_details(View v){
