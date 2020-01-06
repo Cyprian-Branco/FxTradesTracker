94c027d822381b07db9949913bf054fb6ccb333f
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-06 12:13:24 +0300

    add http request GET

--- app/src/main/java/com/moringaschool/fxtradetracker/network/LiveQuotesApi.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/network/LiveQuotesApi.java
@@ -1,4 +1,12 @@
 package com.moringaschool.fxtradetracker.network;
 
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface LiveQuotesApi {
+    @GET("forex/latest")
+    Call<fcsapiResponse> getLiveQuotes(
+            @Query("symbol") String symbol
+    );
+
 }
