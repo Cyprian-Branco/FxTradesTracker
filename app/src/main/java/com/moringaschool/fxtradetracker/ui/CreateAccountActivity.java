1416f3a30b2869646d29822410e98c18546f85fe
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-11 13:41:12 +0300

    fetch create user data

--- app/src/main/java/com/moringaschool/fxtradetracker/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/ui/CreateAccountActivity.java
@@ -2,15 +2,45 @@ package com.moringaschool.fxtradetracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moringaschool.fxtradetracker.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.createUserButton) Button mCreateUserButoon;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButoon.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if(v == mLoginTextView){
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (v == mCreateUserButoon){
+            createNewUser();
+        }
     }
 }
