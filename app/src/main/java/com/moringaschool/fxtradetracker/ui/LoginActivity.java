2ee9b045769481323b84d1595f87cea01c67aafb
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-11 13:26:35 +0300

    add loginactivity and layout

---
+++ app/src/main/java/com/moringaschool/fxtradetracker/ui/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.moringaschool.fxtradetracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.moringaschool.fxtradetracker.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mRegisterTextView){
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+}
