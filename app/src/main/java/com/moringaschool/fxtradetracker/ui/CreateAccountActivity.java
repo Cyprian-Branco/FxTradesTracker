833f9a6399b3ca10cb19dcb0f5f4d6fbd1c5bede
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-11 12:32:39 +0300

    create create acoount page

---
+++ app/src/main/java/com/moringaschool/fxtradetracker/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.fxtradetracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.fxtradetracker.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
