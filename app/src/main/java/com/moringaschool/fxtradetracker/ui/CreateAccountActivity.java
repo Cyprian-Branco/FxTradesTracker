6a88bd41ccff30968f2a4dc4cc177061b408f33f
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-11 13:57:02 +0300

    create user autjentication in firebase

--- app/src/main/java/com/moringaschool/fxtradetracker/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/ui/CreateAccountActivity.java
@@ -1,26 +1,37 @@
 package com.moringaschool.fxtradetracker.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.fxtradetracker.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
     @BindView(R.id.createUserButton) Button mCreateUserButoon;
     @BindView(R.id.nameEditText) EditText mNameEditText;
     @BindView(R.id.emailEditText) EditText mEmailEditText;
     @BindView(R.id.passwordEditText) EditText mPasswordEditText;
     @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @BindView(R.id.loginTextView) TextView mLoginTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,8 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         mLoginTextView.setOnClickListener(this);
         mCreateUserButoon.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
     @Override
     public void onClick(View v){
@@ -42,5 +55,24 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         if (v == mCreateUserButoon){
             createNewUser();
         }
+    }
+    private void createNewUser(){
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Authentication successful");
+                        }else{
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
