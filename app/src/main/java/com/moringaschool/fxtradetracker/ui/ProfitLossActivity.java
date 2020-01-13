29cd7aa2a14c32b249de9829ec2310c15dc024aa
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-13 11:48:42 +0300

    add methods and contsructors for Trade data

--- app/src/main/java/com/moringaschool/fxtradetracker/ui/ProfitLossActivity.java
+++ app/src/main/java/com/moringaschool/fxtradetracker/ui/ProfitLossActivity.java
@@ -5,16 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 import com.moringaschool.fxtradetracker.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfitLossActivity extends Fragment {
+    @BindView(R.id.currencyEditText)
+    EditText mCurrencyEditText;
+    @BindView(R.id.amountEditText) EditText mAmountEditText;
+    @BindView(R.id.buyingEditText) EditText mBuyingEditText;
+    @BindView(R.id.priceEditText) EditText mPriceEditText;
+    @BindView(R.id.volumeEditText) EditText mVolumeEditText;
+    @BindView(R.id.profitLossEditText) EditText mProfitLossEditText;
 
 
     public ProfitLossActivity() {
@@ -26,13 +38,7 @@ public class ProfitLossActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profit_loss, container, false);
-
-       /* Button button = (Button) view.findViewById(R.id.calculateFormButton);
-        button.setOnClickListener(new onClickListener(
-                                  }
-        );
-*/
-
+        ButterKnife.bind(this, view);
 
 return view;
 }
